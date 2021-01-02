a4647f9a58472b40043ff410836cd988544d6e51
Luis Efraín Sánchez Ávila <65471808+Efrain-Sanchez@users.noreply.github.com>
2021-01-01 22:11:21 -0600

    fixed numbered list

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ function getValues() {
 	interes = parseFloat(document.getElementById('interes').value);
 	tiempo = parseFloat(document.getElementById('tiempo').value);
 	monto = parseFloat(document.getElementById('monto').value);
-	mes = 1;
 	mainLogic(monto);
 	function mainLogic (cantidad) {
 		newMonto = ((cantidad * (interes/100)) + cantidad);
 	
-		let items = document.createElement('h1');
-		let contenidoTexto = document.createTextNode(mes + '-. ' + '$' + newMonto.toFixed(3));
+		let items = document.createElement('li');
+		let contenidoTexto = document.createTextNode('$' + newMonto.toFixed(3));
 		
 		items.appendChild(contenidoTexto);
 		mainSection.appendChild(items);
 	};
 	
-	for (i = 1; i <= tiempo; i++) {
+	for (i = 1; i < tiempo; i++) {
 		mainLogic(newMonto);
-		mes++;
 	}
 }
